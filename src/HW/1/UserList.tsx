ff58d6b488ba67aee40b8e50c4325f7ea0741bf5
Zeusus99 <novikov951@gmail.com>
2023-07-21 14:12:20 +0300

    first commit

---
+++ src/HW/1/UserList.tsx
@@ -0,0 +1,32 @@
+type AddressType = {
+  street: string;
+  city: string;
+};
+
+type UserType = {
+  id:number
+  name:string
+  age:number
+  address:AddressType
+};
+
+type UserListPropsType = {
+  users: UserType[]; // ПО МОЕМУ ЧЕГО-ТО НЕ ХВАТАЕТ...
+};
+
+export const UserList = (props: UserListPropsType) => {
+  return (
+    <div id={'hw01-users'}>
+      <h2>User List:</h2>
+
+      <ul>
+        {props.users.map((el) => ( // ВСЕ ТОВАРЫ В СТРАНУ ПРИЕЗЖАЮТ В КОНТЕЙНЕРАХ, А В РЕАКТЕ...
+          <li key={el.id} id={`hw01-user-${el.id}`}>
+            <strong>{el.name}</strong> (Age: {el.age})<strong> Address:</strong>
+            {el.address.street}, {el.address.city}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
